24b95e32ee70a3de70bd95bf224729ee67cf0526
Vitalii Sekreta <81518289+rick-jackson@users.noreply.github.com>
2022-08-15 17:22:00 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
@@ -9,24 +9,30 @@ import { store } from "./store/store";
 function App() {
   const [showNav, setShowNav] = useState(true);
   const [coo, setCoo] = useState(window.pageYOffset);
-  const move = (e) => {
+  const handleScroll = (e) => {
+    // console.log(e)
     let currentScrollPos = window.pageYOffset;
 
     if (coo > currentScrollPos || currentScrollPos === 0) {
-      setShowNav(true)
+      setShowNav(true);
     } else {
-      setShowNav(false)
+      setShowNav(false);
     }
-    setCoo(currentScrollPos)
+    setCoo(currentScrollPos);
   };
 
+  useEffect(() => {
+    document.addEventListener("scroll", handleScroll);
+    document.addEventListener("touch", handleScroll);
+  }, []);
+
   return (
-    <div className="App" onTouchMove={move}>
+    <div className="App">
       <Router>
-      <Provider store={store}>
-        <Navigation showNav={showNav} />
-        <Main />
-        <Footer />
+        <Provider store={store}>
+          <Navigation showNav={showNav} />
+          <Main />
+          <Footer />
         </Provider>
       </Router>
     </div>
