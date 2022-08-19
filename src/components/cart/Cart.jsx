d7d2215cac90cade3ce019a5c43e4f126a61deee
Vitalii Sekreta <81518289+rick-jackson@users.noreply.github.com>
2022-08-19 15:58:59 +0300

    commit

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React  from "react";
 import "./cart.scss";
 import { connect } from "react-redux";
 import { cartSelector } from "../../store/selectors/cart.selector";
--- src/components/productCart/ProductCart.jsx
+++ src/components/productCart/ProductCart.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 import "./productsCart.scss";
 
 const ProductCart = ({ product, removeProduct }) => {
