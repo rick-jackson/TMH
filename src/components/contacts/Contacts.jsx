69b5bf62cc501c64081febda5c3c91d1d029113f
Vitalii Sekreta <81518289+rick-jackson@users.noreply.github.com>
2022-08-20 15:04:16 +0300

    commit

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import './contacts.scss'
+import "./contacts.scss";
 
-
-const Contacts = () =>{
-
+const Contacts = () => {
   return (
     <section className="contacts">
       <div className="contacts__wrapper">
@@ -18,11 +16,31 @@ const Contacts = () =>{
               <li>office@medkommv.com.ua</li>
             </ul>
           </div>
+          <div className="contacts__item">
+            <h5 className="contacts__item-title">ХАРКІВ</h5>
+            <ul className="contacts__item-list">
+              <li>(067) 540 40 90</li>
+              <li>вул. Малопанасівська 4/7, оф. 310</li>
+            </ul>
+          </div>
+          <div className="contacts__item">
+            <h5 className="contacts__item-title">ЛЬВІВ</h5>
+            <ul className="contacts__item-list">
+              <li>(032) 233 10 20</li>
+              <li>(067) 313 64 07</li>
+              <li>вул. Шевченка, 31, оф. 211</li>
+            </ul>
+          </div>
+          <div className="contacts__item">
+            <h5 className="contacts__item-title">ДНІПРО</h5>
+            <ul className="contacts__item-list">
+              <li>(067) 692 12 75</li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-
-export default Contacts
+export default Contacts;
